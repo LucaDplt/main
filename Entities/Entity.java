d45a493559c6e945f4b3d7e22faf5f3ee9443c13
LucaDplt <lucaduplaquet@gmail.com>
2025-11-13 19:21:22 +0100

    Adding the stats to entity + getters and setters

--- Entities/Entity.java
+++ Entities/Entity.java
@@ -12,6 +12,12 @@ public abstract class Entity
     private Classe classe;
     private Race race;
     private Historic historic;
+    private int strength = 0;
+    private int dexterity = 0;
+    private int constitution = 0;
+    private int intelligence = 0;
+    private int wisdom = 0;
+    private int charisma = 0;
     
     //Getters setters
     public String getName()
@@ -63,6 +69,60 @@ public abstract class Entity
     public Historic getHistoric()
     {
         return this.historic;
+    }
+
+    public int getStregnth()
+    {
+        return this.strength;
+    }
+    public void setStrength(int strength)
+    {
+        this.strength = strength;
+    }
+
+    public int getDexterity()
+    {
+        return this.dexterity;
+    }
+    public void setDexterity(int dexterity)
+    {
+        this.dexterity = dexterity;
+    }
+
+    public int getConstitution()
+    {
+        return this.constitution;
+    }
+    public void setConstitution(int constitution)
+    {
+        this.constitution = constitution;
+    }
+
+    public int getIntelligence()
+    {
+        return this.intelligence;
+    }
+    public void setIntelligence(int intelligence)
+    {
+        this.intelligence = intelligence;
+    }
+
+    public int getWisdom()
+    {
+        return this.wisdom;
+    }
+    public void setWisdom(int wisdom)
+    {
+        this.wisdom = wisdom;
+    }
+
+    public int getCharisma()
+    {
+        return this.charisma;
+    }
+    public void setCharisma(int charisma)
+    {
+        this.charisma = charisma;
     }
 
     //Constructor
